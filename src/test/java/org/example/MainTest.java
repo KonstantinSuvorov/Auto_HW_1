c26d4481d41b226418cb70a4c315f0ea28b3d1eb
SkaNic <106460617+KonstantinSuvorov@users.noreply.github.com>
2023-12-15 14:55:48 +0300

    Update MainTest.java

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,4 +1,40 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+
 class MainTest {
-  
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testWinningStrategy() {
+        // Тестирование выигрышной стратегии
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void testLosingStrategy() {
+        // Тестирование проигрышной стратегии
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void testNegativeCases() {
+        // Негативные тесты, например, некорректные входные данные
+        //given
+        //when
+        //then
+    }
 }
